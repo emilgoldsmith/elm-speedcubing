38a930b5e8a8c9dbf833bd02591172554e7af184
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-08-17 16:07:33 +0000

    Fix examples

--- examples/src/CubeViews.elm
+++ examples/src/CubeViews.elm
@@ -5,36 +5,38 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
-cubeSize = 400
+cubeSize =
+    400
+
 
 main =
     div []
         [ div [ style "display" "flex" ]
             [ Cube.view []
                 { pixelSize = cubeSize
-                , cube = Cube.solved
                 , displayAngle = Cube.ufrDisplayAngle
                 , annotateFaces = False
                 }
+                Cube.solved
             , Cube.view []
                 { pixelSize = cubeSize
-                , cube = Cube.solved
                 , displayAngle = Cube.ufrDisplayAngle
                 , annotateFaces = True
                 }
+                Cube.solved
             ]
         , div [ style "display" "flex" ]
             [ Cube.view []
                 { pixelSize = cubeSize
-                , cube = Cube.solved
                 , displayAngle = Cube.ublDisplayAngle
                 , annotateFaces = False
                 }
+                Cube.solved
             , Cube.view []
                 { pixelSize = cubeSize
-                , cube = Cube.solved
                 , displayAngle = Cube.ublDisplayAngle
                 , annotateFaces = True
                 }
+                Cube.solved
             ]
         ]
